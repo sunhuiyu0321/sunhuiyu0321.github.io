27c78919355be70aee07be4c98851c14e9edb666
sunhuiyu0321 <1029956540@qq.com>
2026-09-23 14:53:13 +0800

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,7 +1,6 @@
-//登录校验
 function login(){
-  let user = document.getElementById("username").value;
-  let pwd = document.getElementById("password").value;
+  let user = document.getElementById("login-username").value;
+  let pwd = document.getElementById("login-password").value;
   if(user=="202471022" && pwd=="200622shy"){
     alert("登录成功！");
     window.location.href="index.html";
